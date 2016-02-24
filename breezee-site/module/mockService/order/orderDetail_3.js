c3ec6a0cb5001c9338c4f544aeaf09d908b3c3f2
Shubert.Wang <494297161@qq.com>
2016-02-24 23:22:56 +0800

    order detail

---
+++ breezee-site/module/mockService/order/orderDetail_3.js
@@ -0,0 +1,40 @@
+var data = {
+    id : "3",
+    status : "2",
+    type : 'DineIn',
+    createTime : 1451534400000,
+    code : 'A34',
+    restaurant : {
+        id : "1",
+        name : "索迪斯微软食堂一号"
+    },
+    name : '赵小姐',
+    phone : '18688883456',
+    time : 1451534400000,
+    price : '108.00',
+    food : [
+        {
+            food : {
+                id : 1,
+                name : '农家小炒肉'
+            },
+            number : 1,
+            price : 22.00
+        }
+    ],
+    otherCost : [
+        {
+            id : 1,
+            name : '配送费',
+            number : 1,
+            price : '8.00'
+        },{
+            id : 2,
+            name : '餐盒费',
+            number : 2,
+            price : '2.00'
+        }
+    ]
+};
+
+module.exports = data;
--- breezee-site/routes/viewRoutes/order.js
+++ breezee-site/routes/viewRoutes/order.js
@@ -47,4 +47,19 @@ route.myOrder = function (queryData, res, callback) {
         callback(body);
     });
 };
+route.orderDetail = function (queryData, res, callback) {
+    global.myUtil.request({
+        method : 'get',
+        uri : '/order/',
+        mockData : '/order/orderDetail_'+queryData.id,
+        form : queryData
+    }, function(error, response, body){
+        if(error){
+            throw error;
+        }
+        body.createTime = global.myUtil.dateFormatter(new Date(body.createTime), "yyyy-MM-dd hh:mm");
+        body.time = global.myUtil.dateFormatter(new Date(body.time), "yyyy-MM-dd hh:mm");
+        callback(body);
+    });
+};
 module.exports = route;
