634e9754f7eb7a3d2ee3620c587739f621aa7062
Shubert.Wang <494297161@qq.com>
2016-02-24 22:14:56 +0800

    order list

--- breezee-site/routes/viewRoutes/order.js
+++ breezee-site/routes/viewRoutes/order.js
@@ -32,4 +32,19 @@ route.orderFood = function (queryData, res, callback) {
         callback(body);
     });
 };
+route.myOrder = function (queryData, res, callback) {
+    global.myUtil.request({
+        method : 'get',
+        uri : '/order/',
+        mockData : '/order/myOrder_'+queryData.orderType,
+        form : queryData
+    }, function(error, response, body){
+        if(error){
+            throw error;
+        }
+        body.createTime = global.myUtil.dateFormatter(new Date(body.createTime), "yyyy-MM-dd hh:mm");
+        body.time = global.myUtil.dateFormatter(new Date(body.time), "yyyy-MM-dd hh:mm");
+        callback(body);
+    });
+};
 module.exports = route;
