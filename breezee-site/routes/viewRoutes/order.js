e5c8e7978eb09cac43311791afbb7e1f9fc984a9
Shubert.Wang <494297161@qq.com>
2016-02-24 23:27:18 +0800

    order detail

--- breezee-site/routes/viewRoutes/order.js
+++ breezee-site/routes/viewRoutes/order.js
@@ -2,21 +2,6 @@ var request = require('request');
 
 var route = {};
 
-route.confirmSite = function (queryData, res, callback) {
-    global.myUtil.request({
-        method : 'get',
-        uri : '/restaurant/',
-        mockData : '/order/site',
-        form : queryData
-    }, function(error, response, body){
-        if(error){
-            throw error;
-        }
-        body.createTime = global.myUtil.dateFormatter(new Date(body.createTime), "yyyy-MM-dd hh:mm");
-        body.time = global.myUtil.dateFormatter(new Date(body.time), "yyyy-MM-dd hh:mm");
-        callback(body);
-    });
-};
 route.orderFood = function (queryData, res, callback) {
     global.myUtil.request({
         method : 'get',
