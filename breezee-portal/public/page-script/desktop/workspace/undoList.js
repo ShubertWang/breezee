6a965232216d95316302f0f355fddf7bb9f5b253
fanluo <lu.fing@163.com>
2016-02-28 21:25:14 +0800

    task oms

--- breezee-portal/public/page-script/desktop/workspace/undoList.js
+++ breezee-portal/public/page-script/desktop/workspace/undoList.js
@@ -67,7 +67,7 @@ $(function () {
     page.initEvent = function () {
         var _this = this;
         $("#query").click(function () {
-            this.undoList.query(Dolphin.form.getValue("#queryForm"));
+            _this.undoList.query(Dolphin.form.getValue("#queryForm"));
         });
         $("#conditionReset").click(function () {
             Dolphin.form.empty("#queryForm")
