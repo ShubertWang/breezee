cb4e61d5725e5b89f443914c0367e5db5ee2b45d
Silence <zhong_anjing@163.com>
2016-03-06 14:53:00 +0800

    update the sodex project

--- breezee-portal/public/custom/js/menu.js
+++ breezee-portal/public/custom/js/menu.js
@@ -8,3 +8,7 @@ menu.select = function (code) {
         $('#pageHeader [menuCode="workspace"]').addClass('active');
     }
 };
+
+$(".navMenu").click(function(){
+    Dolphin.goUrl($(this).attr("link"));
+});
