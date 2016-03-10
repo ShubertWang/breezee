f222958ec56fa7587eca86e7d28de7e85f7d6889
Silence <zhong_anjing@163.com>
2016-03-10 18:49:46 +0800

    Merge branch 'master' of https://github.com/AnjingZhong1984/breezee
    # Conflicts:
    #	breezee-site/views/mobile/common/evaluate.ejs

--- breezee-sysmgr/breezee-sysmgr-service/src/main/java/com/breezee/sysmgr/repository/DictRepository.java
+++ breezee-sysmgr/breezee-sysmgr-service/src/main/java/com/breezee/sysmgr/repository/DictRepository.java
@@ -7,8 +7,12 @@ package com.breezee.sysmgr.repository;
 
 import com.breezee.sysmgr.entity.DictEntity;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Silence on 2016/2/10.
@@ -17,4 +21,9 @@ import org.springframework.stereotype.Repository;
 public interface DictRepository extends PagingAndSortingRepository<DictEntity,Long>,JpaSpecificationExecutor<DictEntity> {
 
     DictEntity findByCode(String code);
+
+    @Modifying
+    @Transactional
+    @Query("delete from DictDetailEntity m where m.dict=:dict")
+    void deleteDetails(@Param("dict") DictEntity dict);
 }
